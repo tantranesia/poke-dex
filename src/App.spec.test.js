10b54323aebe9c7218c131c666717e833caf50d8
tantranesia <tantranesia@gmail.com>
2022-08-16 16:22:14 +0700

    testing

--- src/App.spec.test.js
+++ src/App.spec.test.js
@@ -2,6 +2,16 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
 
+// jest.mock('react-redux', () => {
+//     const ActualReactRedux = jest.requireActual('react-redux');
+//     return {
+//         ...ActualReactRedux,
+//         useSelector: jest.fn().mockImplementation(() => {
+//             return mockState;
+//         }),
+//     };
+// });
+
 const greetme = "Hello!";
 describe("Our first test!", () => {
    it("Should say hello!", () => {
